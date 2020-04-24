5c6ab93f76b4eb8ae8bd12d166ae956104fc6d63
masato <mutsuya_maasto@yahoo.co.jp>
2020-04-24 16:27:18 +0900

    3.4.1 Peopleのプロパティ追加、Prefectureの初期データ作成ロジック追加

--- SampleCFModelMVC/Models/Person.cs
+++ SampleCFModelMVC/Models/Person.cs
@@ -9,9 +9,14 @@ namespace SampleCFModelMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
+        public int? Age { get; set; }
         //Prefectureへの外部参照
         public int PrefectureId { get; set; }
         public Prefecture Prefecture { get; set; }
+        public DateTime? HireDate { get; set; }//入社日
+        public bool IsAttend { get; set; }//出退勤
+        public string Email { get; set; }
+        public string Blog { get; set; }
+        public string EmployeeNo { get; set; }//社員番号
     }
 }
