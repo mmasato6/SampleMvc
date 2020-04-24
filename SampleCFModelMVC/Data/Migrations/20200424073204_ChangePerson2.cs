2f4b487d45c769b358ec6426c35e694ac4cdba9d
masato <mutsuya_maasto@yahoo.co.jp>
2020-04-24 16:53:19 +0900

    3.4.2 マイグレーション

---
+++ SampleCFModelMVC/Data/Migrations/20200424073204_ChangePerson2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleCFModelMVC.Data.Migrations
+{
+    public partial class ChangePerson2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Blog",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeNo",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAttend",
+                table: "Person",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Blog",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeNo",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "IsAttend",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
