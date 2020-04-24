9bd1f4885d8657d8216c253b2537b7eb57560e16
masato <mutsuya_maasto@yahoo.co.jp>
2020-04-24 14:45:12 +0900

    OnConfiguringが自動生成により復活していたので削除

--- SampleDBModelMVC/Models/MvcTestDBContext.cs
+++ SampleDBModelMVC/Models/MvcTestDBContext.cs
@@ -18,15 +18,6 @@ namespace SampleDBModelMVC.Models
         public virtual DbSet<Person> Person { get; set; }
         public virtual DbSet<Prefecture> Prefecture { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MvcTestDB;Integrated Security=True");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Person>(entity =>
