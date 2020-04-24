8d78ea4e0a6942e636657a737207acf33a338892
masato <mutsuya_maasto@yahoo.co.jp>
2020-04-24 17:08:14 +0900

    3.4.4 アノテーション追加によるマイグレーション

---
+++ SampleCFModelMVC/Data/Migrations/20200424080415_ChangePersonAnnotation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleCFModelMVC.Data.Migrations
+{
+    public partial class ChangePersonAnnotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Person",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+    }
+}
