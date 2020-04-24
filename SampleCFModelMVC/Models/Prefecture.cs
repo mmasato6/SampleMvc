ec77d6fa5538fb1f4d0f61f3591057464d1c228b
masato <mutsuya_maasto@yahoo.co.jp>
2020-04-24 15:39:04 +0900

    3.3.9 Prefectureクラスの作成

---
+++ SampleCFModelMVC/Models/Prefecture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleCFModelMVC.Models
+{
+    public class Prefecture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
