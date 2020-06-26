9f4464388d3f5ec4154b87e330d30e5f7c510fbc
masato <mutsuya_maasto@yahoo.co.jp>
2020-06-26 18:02:04 +0900

    8.1.3 ～ 8.1.4 EntityFramework関連のクラスを作成

---
+++ SampleWebApi/Data/ApplicationDbConttext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.Data
+{
+    public class ApplicationDbConttext:DbContext
+    {
+        public ApplicationDbConttext(DbContextOptions<ApplicationDbConttext> options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ SampleWebApi/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleWebApi.Models
+{
+    public partial class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int PrefectureId { get; set; }
+
+        public Prefecture Prefecture { get; set; }
+    }
+}
