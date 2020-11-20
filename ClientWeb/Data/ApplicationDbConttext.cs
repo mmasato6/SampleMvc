c64aa2107551a0bd2c19aea11970bd59d30c93e2
masato <mutsuya_maasto@yahoo.co.jp>
2020-11-20 11:37:04 +0900

    8.8.2 jQueryを使うWebアプリケーションを準備する ビルドが通ってホームページが表示されるところまで

---
+++ ClientWeb/Data/ApplicationDbConttext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClientWeb.Models;
+
+namespace ClientWeb.Data
+{
+    public class ApplicationDbConttext:DbContext
+    {
+        public ApplicationDbConttext(DbContextOptions<ApplicationDbConttext> options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<ClientWeb.Models.Prefecture> Prefecture { get; set; }
+    }
+}
---
+++ ClientWeb/Models/Prefecture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientWeb.Models
+{
+    public partial class Prefecture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static void Initialize(DbContext context)
+        {
+            var t = context.Set<Prefecture>();
+            if (!t.Any())
+            {
+                t.AddRange(
+                    new Prefecture() { Name = "北海道" },
+                    new Prefecture() { Name = "青森県" },
+                    new Prefecture() { Name = "東京都" },
+                    new Prefecture() { Name = "静岡県" },
+                    new Prefecture() { Name = "沖縄県" }
+                    );
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
