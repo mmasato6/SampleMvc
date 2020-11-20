53da118029f0c07762117f0ce67a124a3f57d289
masato <mutsuya_maasto@yahoo.co.jp>
2020-11-20 11:43:56 +0900

    8.8.3 Web APIを呼び出すViewページを作成

--- ClientWeb/Controllers/HomeController.cs
+++ ClientWeb/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ClientWeb.Controllers
             return View();
         }
 
+        public IActionResult Test() 
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
